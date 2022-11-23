930576db588a19d87effb893b6c72108b4c705cb
Andrey Acosta <andreyacost24ra18@gmail.com>
2022-11-23 12:56:34 -0600

    Explicacion de la recursividad de las torres de Hanoi

---
+++ cmake-build-debug/CMakeFiles/Tarea_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea_2.dir/main.cpp.obj"
+  "CMakeFiles/Tarea_2.dir/main.cpp.obj.d"
+  "Tarea_2.exe"
+  "Tarea_2.exe.manifest"
+  "Tarea_2.pdb"
+  "libTarea_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -18,9 +18,28 @@ public:
     }
 
     void HANOI(int n, char Sour, char Aux, char Des) {
+        /*
+         *
+         *Determine whether to move only one disk from the first pyramid,
+         * and if so, move the disk to the source tower, which is the first tower,
+         * to the destination tower, which is the last
+         *
+         */
         if (n == 1) {
             cout << "Mover disco " << n << " de " << Sour << " a " << Des << endl;
-        }else{
+        } else {
+            /*
+             *
+             * On the other hand, if we have more than one disk, you must first move the first n disks -1
+             * from the source tower to the auxiliary tower, then you must move the disk left in the source tower
+             * to the destination tower.
+             *Finally, you must move the n disks -1 that are in the auxiliary tower to the destination tower.
+             *
+             *
+             * The formula for moving n disks will always be P(n)= 2*n -1
+             *
+             * Example: moving 7 disks would be so p(7) = 2 * 7 -1 that this equals 127 steps
+             */
             HANOI(n - 1, Sour, Des, Aux);
             cout << "Mover disco " << n << " de " << Sour << " a " << Des << endl;
             HANOI(n - 1, Aux, Sour, Des);
